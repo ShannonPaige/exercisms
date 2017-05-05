91ecd22c623e9e23c8943d5d56f7fc47059c3535
ShannonPaige <spaige@gmail.com>
2017-05-05 10:27:45 -0700

    Add solution for gigasecond in GO

--- go/gigasecond/gigasecond.go
+++ go/gigasecond/gigasecond.go
@@ -1,8 +1,9 @@
 package gigasecond
 
-// import path for the time package from the standard library
 import "time"
 
 const testVersion = 4
 
-func AddGigasecond(time.Time) time.Time
+func AddGigasecond(t time.Time) time.Time {
+	return t.Add(1E9 * time.Second)
+}
