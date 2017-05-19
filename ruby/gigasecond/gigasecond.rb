29c4cb9d525d9ffdf0e71ed6381e2b73938aa370
ShannonPaige <spaige@gmail.com>
2017-05-19 11:24:13 -0700

    Add solution for gigasecond in GO

---
+++ ruby/gigasecond/gigasecond.rb
@@ -0,0 +1,9 @@
+class Gigasecond
+  VERSION = 1
+
+  def self.from(time)
+    gigasecond = 1_000_000_000
+
+    return time += gigasecond
+  end
+end
--- ruby/gigasecond/gigasecond_test.rb
+++ ruby/gigasecond/gigasecond_test.rb
@@ -15,38 +15,32 @@ class GigasecondTest < Minitest::Test
   end
 
   def test_1977_06_13
-    skip
     gs = Gigasecond.from(Time.utc(1977, 6, 13, 0, 0, 0))
     assert_equal Time.utc(2009, 2, 19, 1, 46, 40), gs
   end
 
   def test_1959_07_19
-    skip
     gs = Gigasecond.from(Time.utc(1959, 7, 19, 0, 0, 0))
     assert_equal Time.utc(1991, 3, 27, 1, 46, 40), gs
   end
 
   def test_full_time_specified
-    skip
     gs = Gigasecond.from(Time.utc(2015, 1, 24, 22, 0, 0))
     assert_equal Time.utc(2046, 10, 2, 23, 46, 40), gs
   end
 
   def test_full_time_with_day_roll_over
-    skip
     gs = Gigasecond.from(Time.utc(2015, 1, 24, 23, 59, 59))
     assert_equal Time.utc(2046, 10, 3, 1, 46, 39), gs
   end
 
   # Test your 1Gs anniversary
   def test_with_your_birthday
-    skip
   end
 
   # This test is for the sake of people providing feedback, so they
   # know which version of the exercise you are solving.
   def test_bookkeeping
-    skip
     assert_equal 1, Gigasecond::VERSION
   end
 end
